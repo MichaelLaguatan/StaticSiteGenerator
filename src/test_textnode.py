e6dec25fc58679da2f8a60fa752859745726f7e6
Michael Laguatan <michael.laguatan03@gmail.com>
2025-08-25 17:08:36 -0600

    Created textnode and unit tests to handle markdown text

---
+++ src/test_textnode.py
@@ -0,0 +1,28 @@
+import unittest
+
+from textnode import TextNode, TextType
+from textnode_to_htmlnode import text_node_to_html_node
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.BOLD)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertEqual(node, node2)
+
+    def test_link(self):
+        node = TextNode("This is a link", TextType.LINK, "https://www.google.com")
+        node2 = TextNode("This is a link", TextType.LINK, "https://google.com")
+        self.assertNotEqual(node, node2)
+
+    def test_text_type(self):
+        node = TextNode("This is an image", TextType.IMAGE)
+        node2 = TextNode("This is an image", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_text(self):
+        node = TextNode("This is text", TextType.TEXT)
+        node2 = TextNode("This is code", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+if __name__ == "__main__":
+    unittest.main()
