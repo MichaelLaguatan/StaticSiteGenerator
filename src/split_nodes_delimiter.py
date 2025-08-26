a3fafaae050a119e33592eeebe4c4684056c823e
Michael Laguatan <michael.laguatan03@gmail.com>
2025-08-26 01:25:46 -0600

    Implemented split_nodes_delimiter, need to do unit tests still

---
+++ src/split_nodes_delimiter.py
@@ -0,0 +1,21 @@
+from textnode import TextType, TextNode
+
+def split_nodes_delimiter(old_nodes: list, delimiter: str, text_type: TextType):
+    new_nodes = []
+    for node in old_nodes:
+        if node.text_type != TextType.TEXT:
+            new_nodes.append(node)
+            continue
+        texts = node.text.split(delimiter)
+        for index, text in enumerate(texts):
+            if index % 2 == 0:
+                if text: new_nodes.append(TextNode(text, TextType.TEXT))
+            else: new_nodes.append(TextNode(text, text_type))
+    return new_nodes
+        
+
+
+if __name__ == "__main__":
+    node = TextNode("`code block` This is text with a `code block` word", TextType.TEXT)
+    new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+    print(new_nodes)
---
+++ src/test_split_nodes_delimiter.py
@@ -0,0 +1,10 @@
+import unittest
+
+from textnode import TextNode, TextType
+from textnode_to_htmlnode import text_node_to_html_node
+
+class TestTextNodeToHTMLNode(unittest.TestCase):
+    ...
+    
+if __name__ == "__main__":
+    unittest.main()
